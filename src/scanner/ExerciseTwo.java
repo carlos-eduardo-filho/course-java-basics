23d0002ef866ca119a76bacf383a85ea8acd4f28
Carlos Eduardo <caduvascao12345@gmail.com>
2023-01-13 21:21:31 -0300

    add exercicío 2

---
+++ src/scanner/ExerciseTwo.java
@@ -0,0 +1,15 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int number = scanner.nextInt();
+
+        System.out.println("O número informado foi "+ number);
+
+    }
+}
