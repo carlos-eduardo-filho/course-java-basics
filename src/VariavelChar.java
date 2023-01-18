0e72441b5e15bbecbd0aa912e81e0cf9e66412a8
Carlos Eduardo <caduvascao12345@gmail.com>
2023-01-18 18:46:21 -0300

    add VariaveisChar

---
+++ src/VariavelChar.java
@@ -0,0 +1,14 @@
+public class VariavelChar {
+    public static void main(String[] args) {
+        char Hello = 'H';
+        char Hello01 = 'E';
+        char Hello02 = 'L';
+        char Hello03 = 'L';
+        char Hello04 = 'O';
+
+
+        System.out.println("" + Hello + Hello01 + Hello02 + Hello03 + Hello04);
+
+
+    }
+}
