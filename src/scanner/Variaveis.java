bf2a88661e62427a436eca669f121611743d8b6b
Carlos Eduardo <caduvascao12345@gmail.com>
2023-01-18 18:07:58 -0300

    add VariaveisInteiras

--- src/scanner/Variaveis.java
+++ src/scanner/Variaveis.java
@@ -1,10 +1,16 @@
 public class Variaveis {
     public static void main(String[] args) {
-        int idade = 21;
+        int idade03 = 21;
+        long idade04 = 122_345_678_22l;
         String nome = "Carlos Eduardo";
+        String nomeSeuEstadoCivil = "Se Pegando";
 
-        System.out.println("Idade = " + idade);
-        System.out.println("Nome = " + nome);
+
+        System.out.println("Idade ? = " + idade03);
+        System.out.println("Nome ? = " + nome);
+        System.out.println("Qual é o Seu CPF ? = " + idade04);
+        System.out.println("Qual é seu estado civíl ? = " + nomeSeuEstadoCivil);
+
 
     }
 
