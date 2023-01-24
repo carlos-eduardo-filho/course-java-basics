042b49a03d5ee0fb2d37747e3d7bbc5eef14dda0
Carlos Eduardo <caduvascao12345@gmail.com>
2023-01-24 14:45:36 -0300

    add OperdoresAritimeticos

--- src/LendoDadosDoTeclado.java
+++ src/LendoDadosDoTeclado.java
@@ -4,9 +4,9 @@ public class LendoDadosDoTeclado {
     public static void main(String[] args) {
         Scanner dados = new Scanner (System.in);
 
-        System.out.println("Qual é sua data de nascimento");
-        double data = dados.nextDouble();
-        System.out.println("Sua data de nascimento é: " + data);
+        System.out.println("Qual é o seu nome");
+        String nome = dados.nextLine();
+        System.out.println("Sua nome é: " + nome);
 
         System.out.println("Qual é seu CPF");
         Long cpf = dados.nextLong();
